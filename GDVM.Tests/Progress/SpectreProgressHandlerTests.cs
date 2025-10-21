aafb05c398e23856bc0a9e9c4f5949dfdb39e10e
Patrick T Coakley <contact@patricktcoakley.com>
2025-10-21 11:12:46 -0400

    chore: Fix flakey progress handler tests.

--- GDVM.Tests/Progress/SpectreProgressHandlerTests.cs
+++ GDVM.Tests/Progress/SpectreProgressHandlerTests.cs
@@ -16,6 +16,16 @@ public class SpectreProgressHandlerTests
         };
 
         _testConsole.EmitAnsiSequences();
+
+        // NOTE: Without this, some tests fail in isolation?
+        // TODO: Investigate further
+        var handler = new SpectreProgressHandler<InstallationStage>(_testConsole);
+        handler.TrackProgressAsync(async p =>
+        {
+            p.Report(new OperationProgress<InstallationStage>(InstallationStage.Initializing, "Init"));
+            await Task.Delay(1);
+            return 0;
+        }).Wait();
     }
 
     [Fact]
